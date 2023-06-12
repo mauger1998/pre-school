a3b3e10c736c6d80e0767b252c60c177bde35e1b
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2023-06-12 17:28:33 +0100

    Push price cms

--- index.js
+++ index.js
@@ -13,3 +13,19 @@ document.addEventListener("click", (e) => {
         dropdown.classList.remove("active")
     })
   })
+// CMS
+
+let URL = "https://0pxy7jkz.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%20%3D%3D%20'price'%5D"
+
+fetch(URL)
+.then((res) => res.json())
+.then(({ result }) => {
+
+  if (result.length > 0) {
+    const price = document.querySelector(".main-left p")
+    
+    price.textContent = `£${result[0].price} per hour`
+   
+  }
+})
+.catch((err) => console.error(err));
